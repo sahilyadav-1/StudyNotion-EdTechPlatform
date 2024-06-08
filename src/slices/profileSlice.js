667d6536a213dfc122deaaa996505913cc80a435
sahilyadav-1 <sahil2804yadav@gmail.com>
2024-06-08 15:26:02 +0530

    Deploy

---
+++ src/slices/profileSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  user: null,
+  loading: false,
+}
+// localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : 
+
+const profileSlice = createSlice({
+  name: "profile",
+  initialState: initialState,
+  reducers: {
+    setUser(state, value) {
+      state.user = value.payload
+    },
+    setLoading(state, value) {
+      state.loading = value.payload
+    },
+  },
+})
+
+export const { setUser, setLoading } = profileSlice.actions
+
+export default profileSlice.reducer
